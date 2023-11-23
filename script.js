462c065a03f081303dc1249900e79444ef78d0f0
otaviopedbot <otavio.santos@pedbot.com.br>
2023-11-23 12:29:21 -0300

    update

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 const visor = document.getElementById('visor');
 let digitos = '';
 
-function limitarString(string) {
+function LimitarString(string) {
     if (string.length > 15) {
         return string.substring(string.length - 15);
     }
     return string;
 }
 
-function adicionarAoVisor(valor) {
+function AdicionarAoVisor(valor) {
     digitos += valor;
-    digitos = limitarString(digitos);
+    digitos = LimitarString(digitos);
     visor.innerText = digitos;
 }
 
-function calcular() {
+function Calcular() {
     try {
-        visor.innerText = eval(digitos);
+        novosDigitos = eval(digitos);
+        visor.innerText = novosDigitos;
+        digitos = novosDigitos;
     } catch (error) {
         visor.innerText = 'Erro';
+        digitos = '';
     }
+}
+
+function Limpar() {
+    visor.innerText = '';
     digitos = '';
 }
 
-function limpar() {
-    visor.innerText = '';
-    digitos = '';
+function Apagar() {
+    digitos = digitos.slice(0, -1);
+    visor.innerText = digitos;
 }
